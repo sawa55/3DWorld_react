210b51cb21d4f139e460045eba699d538752ccf3
sawa55 <kouichisawamura5@gmail.com>
2024-05-20 15:24:47 +0900

    video

--- src/Loader.js
+++ src/Loader.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 function Loader({ setGltf, setCardGltf, setCardGltf_a, onLoaded }) {
@@ -25,6 +26,33 @@ function Loader({ setGltf, setCardGltf, setCardGltf_a, onLoaded }) {
 
         loader.load('./Model/Light_bake_3D.glb', (cardGltf_a) => {
             setCardGltf_a(cardGltf_a);
+
+            const video = document.getElementById('video');
+            video.play();
+            const texture = new THREE.VideoTexture(video);
+            texture.minFilter = THREE.LinearFilter;
+            texture.magFilter = THREE.LinearFilter;
+            texture.format = THREE.RGBFormat;
+            texture.wrapS = THREE.RepeatWrapping;  // テクスチャのラッピングを設定
+            texture.repeat.x = -1;  // テクスチャを水平に反転
+
+            const plane = cardGltf_a.scene.children.find(child => child.name === 'Plane');
+            console.log(plane);
+            if (plane) {
+                // MeshBasicMaterialを使用してマテリアルを作成
+                const material = new THREE.MeshBasicMaterial({
+                    map: texture,
+                    side: THREE.DoubleSide,
+                    transparent: true,  // 必要に応じて透明度を設定
+                    opacity: 0.9        // 透明度の値
+                });
+                plane.material = material;
+                plane.material.needsUpdate = true;
+
+                // Planeの上下を反転
+                plane.scale.z = -2;
+            }
+
             checkAllLoaded();
         });
     }, [setGltf, setCardGltf, setCardGltf_a, onLoaded]);
